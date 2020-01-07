f1461abead3d63fc97dd1f09427bc76f1a47cf5a
Joshua T <joshua@sonofone.net>
2020-01-07 16:58:36 -0600

    Begin IntCode in preparation for day 5
    Not quite working yet

---
+++ Day2Rewrite.fsx
@@ -0,0 +1,31 @@
+﻿// Day 2: 1202 Program Alarm
+// https://adventofcode.com/2019/day/2
+//
+// Rewritten to use the IntCode library
+
+#r @"D:\Documents\Projects\AdventOfCode2019FSharp\bin/Debug/netcoreapp3.1/AdventOfCode2019.dll"
+//#r "bin/Debug/netcoreapp3.1/AdventOfCode2019.dll"
+
+let puzzleInput = "1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,13,27,1,10,27,31,2,31,13,35,1,10,35,39,2,9,39,43,2,43,9,47,1,6,47,51,1,10,51,55,2,55,13,59,1,59,10,63,2,63,13,67,2,67,9,71,1,6,71,75,2,75,9,79,1,79,5,83,2,83,13,87,1,9,87,91,1,13,91,95,1,2,95,99,1,99,6,0,99,2,14,0,0"
+
+module Part1 =
+    let checkTestCases() =
+        let check program expected =
+            printfn "\nRunning program: %s" program
+
+            let prgm = IntCode.Program.fromString program
+            match IntCode.Program.run prgm with
+            | IntCode.Complete mem ->
+                let memStr = mem |> IntCode.Program.toString
+                if memStr = expected then
+                    printfn "OK: %s" memStr
+                else
+                    printfn "FAIL: Expected [%s], actual [%s]" expected memStr
+            | _ ->
+                failwithf "Did not successfully complete the program"
+
+        check "1,9,10,3,2,3,11,0,99,30,40,50" "3500,9,10,70,2,3,11,0,99,30,40,50"
+
+        printfn ""
+
+Part1.checkTestCases()
--- IntCode.fs
+++ IntCode.fs
@@ -1,1 +1,120 @@
 ﻿module IntCode
+
+type Memory = int list
+
+type ProgramState =
+| Running of Memory * int
+| Complete of Memory
+
+module private Instruction =
+    type ValidOpCodes =
+    | One of int * int * int
+    | Two of int * int * int
+    | NinetyNine
+
+    let rec private replace v i l = //v - value to substitute, i - index at which to substitute, l - the list
+        // https://stackoverflow.com/a/23482571
+        match i, l with
+        | 0, x::xs -> v::xs //this line does the actual replace
+        | i, x::xs -> x::replace v (i - 1) xs //simply iterates one further through the list
+        | i, [] -> failwith "index out of range" // the given index is outside the bounds of the list
+
+    let getInstruction (mem:Memory) (pointer:int) =
+        let opCode = mem.[pointer]
+
+        try
+            match opCode with
+            | 1 ->
+                One (
+                    mem.[pointer + 1],
+                    mem.[pointer + 2],
+                    mem.[pointer + 3]
+                    )
+            | 2 ->
+                Two (
+                    mem.[pointer + 1],
+                    mem.[pointer + 2],
+                    mem.[pointer + 3]
+                    )
+            | 99 ->
+                NinetyNine
+            | _ ->
+                failwithf "Unknown opcode %i" opCode
+        with
+        | exn -> failwithf "Failed to handle opcode %i at pointer %i: %O" opCode pointer exn
+
+    let invokeInstruction (currentState:ProgramState) (instruction:ValidOpCodes) =
+        match currentState with
+        | Complete _ -> currentState
+        | Running (mem, pointer) ->
+            let newMemory,newPointer =
+                match instruction with
+                | One (x,y,z) ->
+                    let newValue = mem.[x] + mem.[y]
+                    printfn
+                        "Running One instruction:\nX: %i (%i)\nY: %i (%i)\nZ: %i (%i) = %i"
+                        x
+                        mem.[x]
+                        y
+                        mem.[y]
+                        z
+                        mem.[z]
+                        newValue
+
+                    let newMem = mem |> replace newValue mem.[z]
+                    let newPointer = pointer + 4 |> Some
+                    newMem,newPointer
+                | Two (x,y,z) ->
+                    let newValue = mem.[x] * mem.[y]
+                    printfn
+                        "Running Two instruction:\nX: %i (%i)\nY: %i (%i)\nZ: %i (%i) = %i"
+                        x
+                        mem.[x]
+                        y
+                        mem.[y]
+                        z
+                        mem.[z]
+                        newValue
+
+                    let newMem = mem |> replace newValue mem.[z]
+                    let newPointer = pointer + 4 |> Some
+                    newMem,newPointer
+                | NinetyNine ->
+                    mem,None
+
+            match newPointer with
+            | Some s ->
+                printfn
+                    "Completed invokeInstruction:\n\tState: Running\n\tNew memory: [%O]\n\tNew pointer: %i"
+                    (newMemory |> List.map string |> String.concat ",")
+                    s
+                Running (newMemory, s)
+            | None ->
+                printfn
+                    "Completed invokeInstruction:\n\tState:Complete\n\tNew memory: %O"
+                    (newMemory |> List.map string |> String.concat ",")
+                Complete newMemory
+
+module Program =
+    [<Literal>]
+    let private separator = ','
+
+    let fromString (x:string) : Memory =
+        x.Split([|separator|])
+        |> Array.map int
+        |> Array.toList
+
+    let toString (x:Memory) =
+        x
+        |> List.map string
+        |> String.concat (string separator)
+
+    let rec private step state =
+        match state with
+        | Complete _ -> state
+        | Running (memory, pointer) ->
+            Instruction.getInstruction memory pointer
+            |> Instruction.invokeInstruction state
+            |> step
+
+    let run (memory:Memory) = step (Running (memory, 0))
